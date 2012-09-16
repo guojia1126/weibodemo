9ec40dc754afc3a6c56599dffb582ba215c27ddb
Guo Jiaming <176566340@qq.com>
2012-09-16 23:59:02 +0800

    home0916

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
  * GET home page.
  */
 var crypto = require('crypto');
+var User = require('../models/user.js');
 
 exports.index = function(req, res){
   res.render('index', { title: '首页' });
@@ -20,10 +21,27 @@ exports.reg = function(req, res){
 
 exports.doReg = function(req, res){
   console.log(req.body);
+  //生成口令的散列值
   var md5 = crypto.createHash('md5');
   var password = md5.update(req.body.password).digest('base64');
   console.log(password);
-  res.redirect('/reg');
+  var newUser = new User({
+    name: req.body.username,
+    password: password
+  });
+
+  //检查用户名是否已经存在了
+  User.get(newUser.name, function(err, user) {
+    if(user)
+      err = 'Username already exists.';
+    if(err){
+      // req.flash('error', err);
+      return res.redirect('/reg');
+    }
+    req.session.user = newUser();
+    req.flash('success', '注册成功');
+    req.redirect('/');
+  });
 };
 
 exports.login = function(req, res){
